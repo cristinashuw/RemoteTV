62bff81726620fed26d5fe72d180d5cc824415d2
cristinashuw <cristinashuw@gmail.com>
2020-07-27 17:35:04 +0700

    add application class

---
+++ app/src/main/java/com/example/remotetv/RemoteApplication.java
@@ -0,0 +1,11 @@
+package com.example.remotetv;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class RemoteApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
