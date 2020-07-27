3d349fc7d495694f173eac6ce07bda9830bf8c21
cristinashuw <cristinashuw@gmail.com>
2020-07-27 11:12:41 +0700

    update view

--- app/src/main/java/com/example/remotetv/MainActivity.java
+++ app/src/main/java/com/example/remotetv/MainActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
     @Inject
     Remote remote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
