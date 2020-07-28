1dcb4297973a46c2592e3373a6f2c4f33af01e30
cristinashuw <cristinashuw@gmail.com>
2020-07-28 12:09:31 +0700

    @ContributesAndroidInjector

--- app/src/main/java/com/example/remotetv/RemoteModule.java
+++ app/src/main/java/com/example/remotetv/RemoteModule.java
@@ -2,11 +2,16 @@ package com.example.remotetv;
 
 import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class RemoteModule {
 
     @Binds
     abstract Remote remoteSamsung(RemoteSamsung remoteSamsung);
+
+    @ContributesAndroidInjector
+    abstract MainActivity mainActivity();
+
+
 }
