8376ed81ce7e5f981b3d6bea78748104aa65b25a
cristinashuw <cristinashuw@gmail.com>
2020-07-30 09:38:22 +0700

    Adjust Volume Button

--- app/src/main/java/com/example/remotetv/MainActivity.java
+++ app/src/main/java/com/example/remotetv/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.remotetv;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import javax.inject.Inject;
-
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class MainActivity extends DaggerAppCompatActivity {
+    private SeekBar seekBar;
+    private AudioManager audioManager;
 
     @ApplicationScope
     @Inject
@@ -20,7 +25,23 @@ public class MainActivity extends DaggerAppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e("REMOTE LOCATION", remote.toString());
+
+        seekBar = findViewById(R.id.seekBar);
+        audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        seekBar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+
     }
 
+    public void VolUpButton(View view){
+        audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+        seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+        Toast.makeText(this,"Volume Up", Toast.LENGTH_SHORT).show();
+    }
 
+    public void VolDownButton(View view){
+        audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
+        seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+        Toast.makeText(this,"Volume Down", Toast.LENGTH_SHORT).show();
+    }
 }
